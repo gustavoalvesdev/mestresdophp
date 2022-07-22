3aed973367af65a844193ee1bc19cf33584cce08
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2022-07-22 01:12:01 -0300

    Aula 01 PHP Do Zero - Introdução ao Curso Mestres do PHP

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>PHP - Tecnologia Server Side</title>
+    </head>
+    <body>
+        <?php 
+
+            if (1 == 1) {
+                echo 'É número 1';
+            }
+
+        ?>
+    </body>
+</html>
